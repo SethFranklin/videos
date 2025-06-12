787cf005ea58d6d29bf383404a904a675f282472
Seth <sethfranklin@gmail.com>
2025-06-12 19:02:53 -0400

    Added VPC and subnets for cluster

--- argo-cd-on-eks/site/terraform/main.tf
+++ argo-cd-on-eks/site/terraform/main.tf
@@ -15,3 +15,31 @@ provider "aws" {
   region = var.aws_region
 }
 
+locals {
+  VPC_ADDRESS_SPACE = "10.0.0.0/16"
+  AVAILABILITY_ZONES = ["a", "b", "c"]
+
+  subnet_prefix_new_bits = ceil(log(length(local.AVAILABILITY_ZONES), 2))
+  subnet_address_spaces = cidrsubnets(local.VPC_ADDRESS_SPACE, [ for az in local.AVAILABILITY_ZONES : local.subnet_prefix_new_bits ]...)
+}
+
+resource "aws_vpc" "argo_cd" {
+  cidr_block = local.VPC_ADDRESS_SPACE
+
+  tags = {
+    name = "Argo CD VPC"
+  }
+}
+
+resource "aws_subnet" "argo_cd" {
+  for_each = toset(local.AVAILABILITY_ZONES)
+
+  vpc_id     = aws_vpc.argo_cd.id
+  cidr_block = local.subnet_address_spaces[index(local.AVAILABILITY_ZONES, each.key)]
+  availability_zone = "${var.aws_region}${each.key}"
+
+  tags = {
+    Name = "Argo CD Subnet AZ ${each.key}"
+  }
+}
+
